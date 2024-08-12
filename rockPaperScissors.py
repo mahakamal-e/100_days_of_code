d388b0a2bc828af1224f15ec121c24c8de27e785
Maha Kamal <58697984+mahakamal-e@users.noreply.github.com>
2024-08-12 06:12:45 +0300

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -30,7 +30,6 @@ game_images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 computer_choice = random.randint(0, 2)
 
-
 if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number. You lose!")
 else:
@@ -40,8 +39,8 @@ else:
     if user_choice == computer_choice:
         print("It's a draw!")
     elif (user_choice == 0 and computer_choice == 2) or \
-        (user_choice == 1 and computer_choice == 0) or \
-        (user_choice == 2 and computer_choice == 1):
+            (user_choice == 1 and computer_choice == 0) or \
+            (user_choice == 2 and computer_choice == 1):
         print("You win!")
     else:
         print("You lose!")
